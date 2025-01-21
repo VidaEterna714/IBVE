165b4290548c41b04ec40421a1bc121fc2d8ed02
Walter <walterrcruz@yahoo.com>
2025-01-20 16:55:08 -0800

    changed contactenos to contact

--- components/ContactInfo.tsx
+++ components/ContactInfo.tsx
@@ -91,7 +91,7 @@ export default function ContactInfo() {
   return (
     <section className="py-20 ">
       <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold text-center mb-12">Contáctenos</h2>
+        <h2 className="text-4xl font-bold text-center mb-12">Contácto</h2>
         <div className="grid grid-cols-1 filter  md:grid-cols-2 lg:grid-cols-3 gap-8 mb-16">
           {contactData.map((contact, index) => (
             <ContactCard
